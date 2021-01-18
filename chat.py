0b3e2599cee2efddf47921298efb0b1fd5798ac3
ankitkumarakt746 <62370285+ankitkumarakt746@users.noreply.github.com>
2021-01-18 22:19:12 +0530

    close all sockets

--- chat.py
+++ chat.py
@@ -15,24 +15,27 @@ def cleaner():
 		os.system('clear')
 
 def get_localIP():
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.connect(("8.8.8.8", 80))
-    return s.getsockname()[0]
+	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	s.connect(("8.8.8.8", 80))
+	ip = s.getsockname()[0]
+	s.close()
+	return ip
 
 class rec():
-	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	rs = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 	def __init__(self):
 		self._running = True
-		self.s.bind((localIP, localPort))
+		self.rs.bind((localIP, localPort))
 	
 	def stop(self):
 		self._running = False
 
 	def run(self):
 		while self._running:
-			x = self.s.recvfrom(1024)
+			x = self.rs.recvfrom(1024)
 			if x[0].decode() == 'tmnat':
+				self.rs.close()
 				break
 			os.system('tput setaf 2')
 			print('\n' + x[1][0] + ': ' + x[0].decode())
@@ -52,9 +55,11 @@ while True:
 	print("*         Welcome To Phoenix Chat          *")
 	print("*                                          *")
 	print("********************************************")
+	os.system('tput setaf 6')
 	print('\nYour ip: ', localIP)
+	os.system('tput setaf 4')
 	targetIP = input('Enter target IP: ')
-	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	ss = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	print('\nPress 0 to exit.\n')
 	while True:
 		os.system('tput setaf 3')
@@ -62,42 +67,12 @@ while True:
 		if msg == '0' or msg == 'q':
 			os.system('firewall-cmd --remove-port=' + str(localPort) + '/udp > /dev/null 2>&1')
 			recObj.stop()
-			s.sendto('tmnat'.encode(), (localIP, localPort))
+			ss.sendto('tmnat'.encode(), (localIP, localPort))
+			ss.close()
 			os.system('tput setaf 7')
 			cleaner()
 			break
 		else:
-			s.sendto(msg.encode(), (targetIP, targetPort))
+			ss.sendto(msg.encode(), (targetIP, targetPort))
 	break
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-'''def reciever():
-	r = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	r.bind((localIP, localPort))
-	while True:
-		x = r.recvfrom(1024)
-		print(x[0][1] + ': ' + x[0].decode())'''
-
-
-#x1 = threading.Thread(target=reciever)
-	#x1.start()
+	
